7f31b8fb2596727a9f639169df5ff0ba48a5901c
Mark Purcell <markpurcell@ie.ibm.com>
2020-05-21 11:04:59 +0000

    Adding setup for pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MMLL',
+    version='0.1.0',
+    description='Package for interacting with MMLL',
+    author='angelnaviavazquez',
+    author_email='navia@ing.uc3m.es',
+    license='Apache 2.0',
+    packages=find_packages('.'),
+    python_requires='>=3.6',
+    install_requires=[
+        '',
+        '',
+        ''
+    ],
+    url='https://github.com/Musketeer-H2020/MMLL'
+)
