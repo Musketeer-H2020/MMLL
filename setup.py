732ba4d8bc3b366466a61700652eecc53f0459ee
Angel Navia Vazquez <navia@ing.uc3m.es>
2021-01-20 15:23:22 +0100

    MMLL v0.5

--- setup.py
+++ setup.py
@@ -12,13 +12,18 @@ setup(
     install_requires=[
         'transitions==0.6.9',
         'pygraphviz==1.5',
-        'dill',
+        'scipy',
         'scikit-learn',
         'matplotlib',
         'numpy==1.16.4', 
         'Keras==2.2.4',
         'tensorflow==1.14.0',
-        'phe==1.4.0'
+        'phe==1.4.0',
+        'dill==0.3.2',
+        'tqdm==4.50.2',
+        'pympler==0.8',
+        'torchvision==0.8.1',
+        'pillow==7.2.0',
     ],
     url='https://github.com/Musketeer-H2020/MMLL'
 )
